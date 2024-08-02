f063a8e96b8c16e3b22c7106845a9b9bb46ca4d1
Basermc <50556356+Basermc@users.noreply.github.com>
2024-08-02 12:20:08 +0200

    Update process-results.py

--- .github/scripts/process-results.py
+++ .github/scripts/process-results.py
@@ -6,10 +6,12 @@ from datetime import datetime
 
 # Obtener la fecha actual en formato YYYY-MM-DD
 fecha_actual = datetime.now().strftime('%Y-%m-%d')
-directorio_puntuaciones = f'puntuaciones/{fecha_actual}'
-directorio_resultados = f'resultados/{fecha_actual}'
 
-# Crear directorios si no existen
+# Directorios
+directorio_puntuaciones = 'puntuaciones'
+directorio_resultados = 'resultados'
+
+# Crear directorio si no existe
 os.makedirs(directorio_resultados, exist_ok=True)
 
 # Ruta a los archivos txt
@@ -43,21 +45,11 @@ df_puntuaciones = df.explode('puntuaciones').reset_index(drop=True)
 # Agrupar por nombre y sumar las puntuaciones
 df_grouped = df_puntuaciones.groupby('name').sum().reset_index()
 
-# Ordenar por total y seleccionar los 32 mejores freestylers
-df_top_32 = df_grouped.sort_values(by='total', ascending=False).head(32)
+# Ordenar por total y seleccionar los 32 mejores
+df_sorted = df_grouped.sort_values(by='total', ascending=False).head(32)
 
-# Guardar los resultados agrupados y los mejores freestylers
-output_file_grouped = os.path.join(directorio_resultados, 'resultados_agrupados.csv')
-output_file_top_32 = os.path.join(directorio_resultados, 'best_freestylers.txt')
+# Guardar resultados
+output_file = f'{directorio_resultados}/{fecha_actual}.txt'
+df_sorted.to_csv(output_file, index=False, header=False, sep=' ', mode='w')
 
-# Guardar el DataFrame agrupado como CSV
-df_grouped.to_csv(output_file_grouped, index=False)
-
-# Guardar los mejores 32 freestylers como TXT
-with open(output_file_top_32, 'w') as f:
-    for index, row in df_top_32.iterrows():
-        f.write(f"{row['name']}\t{row['total']}\n")
-
-print(f'Resultados guardados en {output_file_grouped}')
-print(f'Mejores 32 freestylers guardados en {output_file_top_32}')
-print('Archivos creados en el directorio:', directorio_resultados)
+print(f"Archivo guardado en {output_file}")
